be33f3e8bda898191f48e79567428dcd8496a7a3
Ta Minh Tien <taminhtien1993@gmail.com>
2015-07-19 15:16:00 +0700

    Add recipe Model and View

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'haml', '~> 4.0.6'
-
+gem 'simple_form', '~> 3.1.0'
+gem 'bootstrap-sass', '~> 3.2.0.2'
 gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   gem 'byebug'
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,47 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
+  # GET /users
   def index
+    @recipes = Recipe.all.order("created_at DESC")
   end
 
+  # GET /users/1
   def show
   end
 
+  # GET /users/new
   def new
     @recipe = Recipe.new
   end
 
+  # POST /users
   def create
     @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: "Successfully created new recipe"
+    elsif
+      render 'new'
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # PATCH /users/1
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "Successfully updated recipe"
+    else
+      render 'edit'
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Successfully deleted recipe"
   end
 
   private
